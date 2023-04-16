fa580744c1ebe9f77d41329fa60ca35f2239bab9
Kacper <kacper.markowski2@teb.edu.pl>
2023-04-16 17:03:05 +0200

    naprawienie fibonaciego rekurencyjnego oraz usunięcie soutów w pętlach w mainie

--- src/Fibonaci.java
+++ src/Fibonaci.java
@@ -9,16 +9,15 @@ public class Fibonaci {
         return liczba_koncowa;
     }
     private static int i=0;
-    private static int liczba_startowa=1;
-    public static int fib_rek(int n, int wynik){
+    private static int wynik=0;
+    public static int fib_rek(int n, int liczba_startowa){
         if(i<n){
             i++;
             wynik=wynik+liczba_startowa;
             liczba_startowa=wynik-liczba_startowa;
-            return fib_rek(n, wynik);
+            return fib_rek(n, liczba_startowa);
         }
         return wynik;
     }
-
-
+    //nie wiem dlaczego tu trzeba robić zmienną poza funkcją(wynik) a nie może być podane jako argument funkcji bo nie działa
 }
--- src/Main.java
+++ src/Main.java
@@ -6,34 +6,39 @@ public class Main {
         int n;
         System.out.println("Podaj do którego miejsca chcesz wykonać: ");
         n=in.nextInt();
+        //-------------------------------------------------------
         long startIt=System.nanoTime();
         for (int i = 0; i < 100; i++) {
-            System.out.println(n+" liczba ciagu fibonaciego to: "+Fibonaci.fib_it(n));
+            Fibonaci.fib_it(n);
         }
         long finishIt=System.nanoTime();
+        System.out.println(n+" liczba ciagu fibonaciego to: "+Fibonaci.fib_it(n));
+        //-------------------------------------------------------
         long startRek=System.nanoTime();
         for (int i = 0; i < 100; i++) {
-            System.out.println(n+" liczba ciagu fibonaciego to: "+Fibonaci.fib_rek(n, 0));
+            Fibonaci.fib_rek(n,1);
         }
         long finishRek=System.nanoTime();
+        System.out.println(n+" liczba ciagu fibonaciego to: "+Fibonaci.fib_rek(n,1));
+        //-------------------------------------------------------
         long startIt2=System.nanoTime();
         for (int i = 0; i < 100; i++) {
-            System.out.println("Silnia od liczby "+n+" to: "+Silnia.sil_it(n));
+            Silnia.sil_it(n);
         }
         long finishIt2=System.nanoTime();
+        System.out.println("Silnia od liczby "+n+" to: "+Silnia.sil_it(n));
+        //-------------------------------------------------------
         long startRek2=System.nanoTime();
         for (int i = 0; i < 100; i++) {
-            System.out.println("Silnia od liczby "+n+" to: "+Silnia.sil_rek(n,1));
+            Silnia.sil_rek(n,1);
         }
         long finishRek2=System.nanoTime();
+        System.out.println("Silnia od liczby "+n+" to: "+Silnia.sil_rek(n,1));
+        //-------------------------------------------------------
+        System.out.println("---------------------------------------------");
         System.out.println("czas fibonaci it:  "+(finishIt-startIt));
         System.out.println("czas fibonaci rek: "+(finishRek-startRek));
         System.out.println("czas silnia it:  "+(finishIt2-startIt2));
         System.out.println("czas silnia rek: "+(finishRek2-startRek2));
-
-        for (int i = 0; i < 100; i++) {
-
-        }
-
     }
 }
